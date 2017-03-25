16db5995abaa8a3f7359d1da0a254f6cd72321c7
Marcin Bielak <marcin.bieli@gmail.com>
2017-03-25 11:47:06 +0100

    task code execution from web (security + show errors from AJAX)

--- mainapp/views.py
+++ mainapp/views.py
@@ -190,34 +190,38 @@ class ContetUserStatusView(View):
 
 
 class TaskCodeRunView(View):
-    def post(self, request):
-        if not request.user.is_authenticated():
-            return HttpResponse(status=401)
+    def _security_check(self, result):
+        print("result: ", result)
+        if 'import ' in result:
+            print("AAAAAAa\n\n")
+            return False
 
-        if not request.is_ajax():
-            return HttpResponseBadRequest()
+        if ' loader' in result:
+            return False
 
+        # if 'os.' in result:
+        #     return False
+        #
+        # if 'sys.' in result:
+        #     return False
 
-        content_type = request.META['CONTENT_TYPE'].replace('application/x-www-form-urlencoded; charset=', '')
-        # print('content_type: {}'.format(content_type))
-        user_id = request.POST.get('user_id', None)
-        # print('user_id: {}'.format(user_id))
-        task_id = request.POST.get('task_id', None)
-        # print('task_id: {}'.format(task_id))
-        codeb64 = request.POST.get('code', None)
-        code = base64.decodebytes(bytes(codeb64.encode(content_type)))
-        print('code: {}'.format(code))
-        testsb64 = request.POST.get('tests', None)
-        tests = base64.decodebytes(bytes(testsb64.encode(content_type)))
-        print('tests: {}'.format(tests))
+        return True
 
+    def _prepare_result(self, result, tmp_filename, replaced_filename='program.py'):
+        result = result.decode("utf-8")
+        result = result.replace(tmp_filename, replaced_filename)
+        # if 'AssertionError' in result and ' assert ' in result:
+        #     assert_pos = result.index(' assert ')
+        #     result = result[assert_pos:].replace('AssertionError', '').strip()
+        return str(result)
+
+    def run_python_code(self, code, task_id, tests, user_id):
         from subprocess import Popen, PIPE
         # cmd = "python3.4 test003.py"
         # p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         # out, err = p.communicate()
         # print("Return code: ", p.returncode)
         # print(out.rstrip(), err.rstrip())
-
         result = ''
         main = """
 # main
@@ -225,9 +229,9 @@ if __name__ == '__main__':
     __run_tests()
 # main
         """
-
         hash = hashlib.sha1(bytes(code)).hexdigest()
         tmp_filename = "/tmp/{}-{}-{}-{}".format(user_id, task_id, time.time(), hash)
+        # tmp_filename = "{}/tmp/{}-{}-{}-{}".format(settings.STATIC_ROOT, user_id, task_id, time.time(), hash)
         with open(tmp_filename, 'w+') as tmpfile:
             tmpfile.write(code.decode('latin-1'))
             tmpfile.write("\n\n")
@@ -239,61 +243,65 @@ if __name__ == '__main__':
             tmpfile.flush()
             tmpfile.close()
 
-            try:
-                cmd = "%s %s" % ("/usr/bin/python3.4", tmp_filename)
-                p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-                out, err = p.communicate()
-                print("Return code: ", p.returncode)
-                print(out.rstrip(), err.rstrip())
-                result = out.rstrip() + err.rstrip()
-            except:
-                result = 'ERR: 2'
 
-            # try:
-            #     os.unlink(tmp_filename)
-            # except:
-            #     pass
+            result = ''
+            if not self._security_check(str(code)):
+                result = "Unexpected instructions !\nPlease rewrite your code and run again !"
+            else:
+                try:
+                    cmd = "%s %s" % ("/usr/bin/python3.4", tmp_filename)
+                    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+                    out, err = p.communicate()
+                    print("Return code: ", p.returncode)
+                    print(out.rstrip(), err.rstrip())
+                    result = out.rstrip() + err.rstrip()
+                except:
+                    result = 'ERR: 2'
 
-            def prepare_result(result, tmp_filename, replaced_filename='program.py'):
-                result = result.decode("utf-8")
-                result = result.replace(tmp_filename, replaced_filename)
-                # if 'AssertionError' in result and ' assert ' in result:
-                #     assert_pos = result.index(' assert ')
-                #     result = result[assert_pos:].replace('AssertionError', '').strip()
-                return result
+                # try:
+                # os.unlink(tmp_filename)
+                # except:
+                #     pass
 
-            result = prepare_result(result, tmp_filename)
+                result = self._prepare_result(result, tmp_filename)
+
+        return result
+
+    def post(self, request):
+        if not request.user.is_authenticated():
+            return HttpResponse(status=401)
+
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+
+        user_id = request.POST.get('user_id', None)
+        # print('user_id: {}'.format(user_id))
+
+        if User.objects.filter(id__iexact=user_id).exists():
+            # print('user EXISTS')
+
+            if int(request.user.id) != int(user_id):
+                # print('request.user.id != user_id')
+                return HttpResponseBadRequest('wrong auth user in request')
+
+            content_type = request.META['CONTENT_TYPE'].replace('application/x-www-form-urlencoded; charset=', '')
+            # print('content_type: {}'.format(content_type))
+            task_id = request.POST.get('task_id', None)
+            # print('task_id: {}'.format(task_id))
+            codeb64 = request.POST.get('code', None)
+            code = base64.decodebytes(bytes(codeb64.encode(content_type)))
+            print('code: {}'.format(code))
+            testsb64 = request.POST.get('tests', None)
+            tests = base64.decodebytes(bytes(testsb64.encode(content_type)))
+            print('tests: {}'.format(tests))
+
+            result = self.run_python_code(code, task_id, tests, user_id)
+        else:
+            # print('user NOT EXISTS')
+            return HttpResponseBadRequest('user NOT EXISTS')
 
         return HttpResponse(result)
 
-        # print('is_authenticated: {}'.format(request.user.is_authenticated()))
-        # print('Raw Data: {}'.format(request.body))
-        # print('Raw User Id: {}'.format(request.user.id))
-        # print('request.is_ajax(): {}'.format(request.is_ajax()))
-        #
-        # if User.objects.filter(id__iexact=user_id).exists():
-        #     print('user EXISTS')
-        #
-        #     if int(request.user.id) != int(user_id):
-        #         # print('request.user.id != user_id')
-        #         return HttpResponseBadRequest('wrong auth user in request')
-        #
-        #     user_instance = User.objects.filter(id=user_id).get()
-        #     content_instance = Content.objects.filter(id=content_id).get()
-        #
-        #     obj = ContentStatus.objects.filter(user=user_instance, content=content_instance)
-        #     if obj:
-        #         cs = obj.get()
-        #         # print('content - id {}, data: {}'.format(content_id, cs))
-        #         # print('status: {}'.format(cs.status))
-        #         return HttpResponse(cs.status)
-        #         #return HttpResponse('OK')
-        #     else:
-        #         # print('content NOT EXISTS')
-        #         return HttpResponseBadRequest('content NOT EXISTS')
-        # else:
-        #     # print('user NOT EXISTS')
-        #     return HttpResponseBadRequest('user NOT EXISTS')
 
 
 class ContetUserView(View):
